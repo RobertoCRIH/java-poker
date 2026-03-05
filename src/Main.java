1103217318db35ccc37fce24adf6ab68e2b57911
RobertoCRIH <108534863+RobertoCRIH@users.noreply.github.com>
2026-03-04 19:31:23 -0600

    tres nuevas manos

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,24 @@ public class Main {
     public static void main(String[] args){
         //Este programa toma una mano de poker y nos dice que tipos tiene.
         //H = Hearts  C = Clovers  S = Spades  D = Diamonds
+        //J = Joto  Q = Cuina  R = Rey  D = Diez
 
         //Esta es la mano que tiene el jugador.
         ArrayList<String> myHand = new ArrayList<String>();
 
         myHand.add("2H");
-        myHand.add("4H");
-        myHand.add("QD");
-        myHand.add("QS");
-        myHand.add("QC");
+        myHand.add("DH");
+        myHand.add("RH");
+        myHand.add("DH");
+        myHand.add("RC");
 
         myHand.sort(null);
-
-        System.out.println(hasThreeOfKind(myHand));
+        System.out.println(hasFullHouse(myHand));
 
     }
 
 
-    //Checa si hay un par dentro de la mano. Regresa las dos cartas que conforman el par
+    ///Checa si hay un par dentro de la mano. Regresa las dos cartas que conforman el par
     private static ArrayList<String> hasPair( ArrayList<String> hand ){
         //La respuesta que se va a regresar
         ArrayList<String> response = new ArrayList<String>();
@@ -48,7 +48,7 @@ public class Main {
         return response;
     }
 
-    //Chaca si hay dos pares en la mano. Regresa una lista de 4 cartas.
+    ///Checa si hay dos pares en la mano. Regresa una lista de 4 cartas.
     private static ArrayList<String> hasTwoPair( ArrayList<String> hand ){
 
         ArrayList<String> response = new ArrayList<String>();
@@ -78,6 +78,7 @@ public class Main {
         return response;
     }
 
+    ///Checa si hay una tercia. Regresa una lista con las tres cartas.
     private static ArrayList<String> hasThreeOfKind( ArrayList<String> hand){
         ArrayList<String> response = new ArrayList<String>();
 
@@ -100,5 +101,95 @@ public class Main {
         return response;
     }
 
+    ///Checa si tiene un straight y regresa un array de cinco cartas.
+    private static ArrayList<String> hasStraight( ArrayList<String> hand ){
+        //Este es el orden de la mano straight
+        String straightOrder = "23456789DJQR";
 
+        //Guardamos si la mano tiene un As o no.
+        boolean hasAce = false;
+
+        ArrayList<String> response = new ArrayList<String>();
+
+        //Acomodamos la mano segun orden alfabetico
+        hand.sort(null);
+
+        //Convertimos la mano en un string para compararlo con el orden.
+        String handString = "";
+
+        //Agregamos todos los valores dentro del string.
+        for (int i = 0; i < hand.size(); i++) {
+            char card = hand.get(i).charAt(0);
+
+            //Si tenemos un As no se agrega a la mano.
+            //El As lo checamos luego, porque puede ubicarse al principio o final del string.
+            if (card != 'A'){
+                handString = handString + card ;
+            } else {
+                hasAce = true;
+            }
+
+        }
+
+        if (hasAce){
+            if ( (handString.equals("2345")) || (handString.equals("DJQR")) ){
+                System.out.println("Straight with Ace!");
+                response = hand;
+                return response;
+            }
+        } else if (straightOrder.contains(handString)) {
+            System.out.println("Straight!");
+            response = hand;
+        }
+
+        return response;
+
+    }
+
+    /// Checa si todas las cartas son del mismo palo. Regresa una lista de cinco si la respuesta es positiva.
+    private static ArrayList<String> hasFlush( ArrayList<String> hand ){
+        //Tomamos el tipo de la primera carta y comparamos que los demas sean iguales.
+        char handType = hand.getFirst().charAt(1);
+
+        for (int i = 1; i < hand.size(); i++) {
+            //El tipo de carta que vamos a comparar.
+            char cardType = hand.get(i).charAt(1);
+
+            //Si el tipo no es igual, mandamos un array vacio para acabar con la funcion.
+            if ( cardType != handType ){
+                return new ArrayList<String >();
+            }
+        }
+
+        //Si no se encuentra nada malo regresa la mano inicial.
+        return hand;
+    }
+
+    /// Checa si la mano tiene un par y una tercia. Si lo tiene, regresa una lista de cinco elementos.
+    private static ArrayList<String> hasFullHouse( ArrayList<String> hand ){
+
+
+        //Checamos si hay una tercia.
+        ArrayList<String> threeOfKind = hasThreeOfKind(hand);
+
+        //Si no hay un tercio, esta mal y regresamos un list vacio.
+        if (threeOfKind.isEmpty()){
+            return new ArrayList<String>();
+        }
+
+        hand.removeAll(threeOfKind);
+
+        //Checamos si hay un par.
+        ArrayList<String> pair = hasPair(hand);
+
+        //Si esta vacio regresamos respuesta negativa.
+        if (pair.isEmpty()) {
+            return new ArrayList<String>();
+        }
+
+        //Agregamos a la tercia el par para poder mandar las cinco cartas juntas de respuesta.
+        threeOfKind.addAll(pair);
+
+        return threeOfKind;
+    }
 }
